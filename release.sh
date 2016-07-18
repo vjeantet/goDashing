64bc39170792153bc31781fa4a94850650a11fb3
Valere JEANTET <valere.jeantet@gmail.com>
2016-07-18 20:41:31 +0200

    build for used binaries first

--- release.sh
+++ release.sh
@@ -10,6 +10,14 @@ rm ${releaseFolder}*.zip
 rice embed-go
 
 arch=amd64
+os=windows
+product=${productName}_${versionLabel}_${os}_${arch}
+env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe cmd/main.go
+zip -r ${product}.zip ${product}.exe
+rm $product.exe
+mv $product.zip ${releaseFolder}/
+
+arch=amd64
 os=darwin
 product=${productName}_${versionLabel}_${os}_${arch}
 env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product cmd/main.go
@@ -17,6 +25,13 @@ tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
 
+arch=arm
+os=linux
+product=${productName}_${versionLabel}_${os}_${arch}
+env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product cmd/main.go
+tar czfv $product.tgz $product
+rm $product
+mv $product.tgz ${releaseFolder}/
 
 arch=amd64
 os=linux
@@ -33,22 +48,6 @@ env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.
 tar czfv $product.tgz $product
 rm $product
 mv $product.tgz ${releaseFolder}/
-
-arch=arm
-os=linux
-product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o $product cmd/main.go
-tar czfv $product.tgz $product
-rm $product
-mv $product.tgz ${releaseFolder}/
-
-arch=amd64
-os=windows
-product=${productName}_${versionLabel}_${os}_${arch}
-env GOOS=$os GOARCH=$arch go build -ldflags="-s -w -X main.version=${1} -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` " -o ${product}.exe cmd/main.go
-zip -r ${product}.zip ${product}.exe
-rm $product.exe
-mv $product.zip ${releaseFolder}/
 
 arch=386
 os=windows
